4ba3fe2b63e0ed2d78ae2fdf560a7e03898846e6
alaamigdady <36266762+alaamigdady@users.noreply.github.com>
2019-02-12 01:00:17 +0200

    Create main.php

---
+++ main.php
@@ -0,0 +1,28 @@
+
+<html>
+	<body>
+		<h1> Levenshtien  Distance </h1>
+		<br>
+		<br>
+		<form action="leven.php" method="get">
+			<input placeholder='first string' type='text' name="lev_str1" />
+			<input placeholder= 'second sring' type ='text' name ="lev_str2" />
+			<br>
+			<br>
+			<input type="submit"/>
+		</form>
+		<br>
+		<br>
+		<h1> Hamming  Distsance </h1>
+		<br>
+		<br>
+		<form action="hamming.php" method="get">
+			<input placeholder='first string' type='text' name="ham_str1" />
+			<input placeholder= 'second sring' type ='text' name ="ham_str2" />
+			<br>
+			<br>
+			<input type="submit"/>
+		</form>
+	</body>
+</html>
+

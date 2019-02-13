eed3f7dcbc7fd0b3900773da360ee89d7ed0643b
alaamigdady <36266762+alaamigdady@users.noreply.github.com>
2019-02-14 00:35:58 +0200

    Update cmd.php

--- cmd.php
+++ cmd.php
@@ -1,19 +1,19 @@
 <?php
   include 'leven.php'; // include Levenshtien class
 
-  $stdin = fopen('php://stdin', 'r');
+  $stdin = fopen('php://stdin', 'r');// create a file to handle stdin
 
   echo "enter the first string :\n";
 
-  $str1 = fgets($stdin);
+  $str1 = fgets($stdin); // get the first srting
 
   echo "enter the second string : \n";
 
-  $str2 = fgets($stdin);
+  $str2 = fgets($stdin); // get the second string
 
-  $distance = Levenshtein::helper($str1, $str2);
+  $distance = Levenshtein::helper($str1, $str2); // calling the function and get the result
 
   echo "minimun ditance needed is : ".$distance; ;
 
-  fclose($stdin);
+  fclose($stdin); 
 ?>

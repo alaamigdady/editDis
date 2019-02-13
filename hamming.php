9bfbfcceb72e37876e05240a17b9fc51c34ee5d7
alaamigdady <36266762+alaamigdady@users.noreply.github.com>
2019-02-14 00:27:27 +0200

    Update hamming.php

--- hamming.php
+++ hamming.php
@@ -1,35 +1,39 @@
-<?php
 
+<?php
 class Hamming {
 
 	private $count=0;
 	private $str1 ;
-	  private $str2 ;
-    function __construct() {
-        $this->str1 = isset($_GET['ham_str1']) ? $_GET['ham_str1'] : '';
-        $this->str2 = isset($_GET['ham_str2']) ? $_GET['ham_str2'] : '';
+	private $str2 ;
+
+    private function __construct($str1,$str2) {
+        $this->str1 = $str1;
+        $this->str2 = $str2;
 
     }
 
-	
-	public function editDistace (){
-		if(strlen($this->str1)!= strlen($this->str2)){
-			echo "two strings must have same length";
-		}else{
+    public static function helper ($str1,$str2){
+    	$hamming = new Hamming($str1,$str2);
+    	$hamming->calculateDistnace();
+    	return $hamming->count;
+    }
+
+	private function calculateDistnace (){
+		if(strlen($this->str1)< strlen($this->str2)){
+			$this->str1 = str_pad($this->str1,strlen($this->str2)," ");
+		}elseif (strlen($this->str2)< strlen($this->str1)){
+			$this->str2 = str_pad($this->str2,strlen($this->str1)," ");
+
+		}
+
 			for($i=0; $i<strlen($this->str1);$i++){
 			if($this->str1[$i] !== $this->str2[$i]){
 				$this->count = $this->count +1;
 			}
 		}
-		return $this->count;
-
-		}
-		
+		return $this->count;		
 	}
 }
 
-$ham = new Hamming();
-
-echo $ham->editDistace()
 
 ?>

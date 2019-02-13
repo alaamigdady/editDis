42bf8427d69ac71b7a6e0de4a2d64c4c240e190f
alaamigdady <36266762+alaamigdady@users.noreply.github.com>
2019-02-14 00:29:07 +0200

    Update main.php

--- main.php
+++ main.php
@@ -1,28 +1,76 @@
 
+<?php
+	include 'leven.php';
+	include 'hamming.php';
+?>
+
+<!DOCTYPE html>
 <html>
+	<head>
+		<title>Edit Distance</title>
+	</head>
+
+	<style>
+		body{
+			background-color:LightSlateGray;
+			height:100%;
+			display:table-cell; 
+			text-align:center; 
+			vertical-align:middle;
+		}
+	 html{
+		 height:100%;
+		 display:table; 
+		 width:100%;
+		}
+	</style>
+
 	<body>
-		<h1> Levenshtien  Distance </h1>
+		      
+		<h3> Levenshtien  Distance </h3>
 		<br>
 		<br>
-		<form action="leven.php" method="get">
+		<form action="" method="get">
 			<input placeholder='first string' type='text' name="lev_str1" />
 			<input placeholder= 'second sring' type ='text' name ="lev_str2" />
 			<br>
 			<br>
-			<input type="submit"/>
+			<input type="submit" name="levenshtien"/>
 		</form>
 		<br>
 		<br>
-		<h1> Hamming  Distsance </h1>
+
+		<?php
+			if( isset($_GET['levenshtien']) ){
+				$str1 = isset($_GET['lev_str1']) ? $_GET['lev_str1'] : '';
+				$str2 = isset($_GET['lev_str2']) ? $_GET['lev_str2'] : '';
+				$distance = Levenshtein::helper($str1, $str2);
+	       		echo " <br> minimum distance neede is : " .$distance;	
+			}
+		?>
+
+		<h3> Hamming  Distsance </h3>
 		<br>
 		<br>
-		<form action="hamming.php" method="get">
+		<form action="" method="get">
 			<input placeholder='first string' type='text' name="ham_str1" />
 			<input placeholder= 'second sring' type ='text' name ="ham_str2" />
 			<br>
 			<br>
-			<input type="submit"/>
+			<input type="submit" name="hamming"/>
 		</form>
+
+		<?php
+			
+			if( isset($_GET['hamming']) ){
+				$str1 = isset($_GET['ham_str1']) ? $_GET['ham_str1'] : '';
+				$str2 = isset($_GET['ham_str2']) ? $_GET['ham_str2'] : '';
+				$distance = Hamming::helper($str1, $str2);
+				echo " <br> minimum distance neede is : " .$distance;
+			}
+		?>
 	</body>
 </html>
+
+
 
